de2fa190bdf8ff2f53a92713ccb094889938af1e
Quentin Brunet <brunet_quentin@outlook.fr>
2024-02-09 12:23:49 +0100

    modification

---
+++ match.php
@@ -0,0 +1,53 @@
+<?php
+include_once("./inc/pdo.php");
+$email = $_GET['email'];
+$rq2 = "SELECT * FROM `user` WHERE email = '$email'";
+    $read = $pdo->prepare($rq2);
+    $read->execute();
+    $result = $read->fetch();
+    $titres = [
+        $result['titre1'],
+        $result['titre2'],
+        $result['titre3'],
+        $result['titre4'],
+        $result['titre5']
+    ];
+    $sexe = $result['sexe'];
+     $resultGlobal = [];
+    foreach ($titres as $key => $value) {
+        $rq3 = "SELECT * FROM user WHERE email != '$email'
+        AND titre1 = '$value' OR titre2= '$value' OR titre3= '$value' OR titre4= '$value' OR titre5= '$value'";
+        $readTitres = $pdo->prepare($rq3);
+        $readTitres->execute();
+        $resultTitres = $readTitres->fetchAll();
+        
+        foreach ($resultTitres as $key => $value) {
+            if ($result['id_user'] !== $value) {
+                array_push($resultGlobal, $value);
+            }
+        }
+    }
+$resultGlobal = array_unique($resultGlobal, SORT_REGULAR);
+var_dump($resultTitres);
+
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Page de match</h1>
+    <div><?= $result['titre1'] ?></div>
+    <div><?= $result['titre2'] ?></div>
+    <div><?= $result['titre3'] ?></div>
+    <div><?= $result['titre4'] ?></div>
+    <div><?= $result['titre5'] ?></div>
+</body>
+</html>
---
+++ registerend.php
@@ -0,0 +1,17 @@
+<?php
+var_dump($_POST);
+
+include_once("./inc/pdo.php");
+$email = $_POST["email"];
+$titre1 = $_POST["titre1"];
+$titre2 = $_POST["titre2"];
+$titre3 = $_POST["titre3"];
+$titre4 = $_POST["titre4"];
+$titre5 = $_POST["titre5"];
+//securité
+$rq = "UPDATE `user` SET `titre1`='$titre1',`titre2`='$titre2',`titre3`='$titre3',`titre4`='$titre4',`titre5`='$titre5' WHERE email = '$email'";
+$update = $pdo->prepare($rq);
+$update->execute();
+header("Location: ./match.php?email=".$email);
+?>
+
